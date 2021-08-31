801856683077f1c45eaddb825754ab9401acaa38
Damon Vu (bav965) <damon.vu@usask.ca>
2021-08-30 18:40:27 -0600

    refactor logics from controller -> service

--- PPMToolFullStack/src/main/java/com/damon/vu/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/com/damon/vu/ppmtool/web/ProjectController.java
@@ -1,6 +1,7 @@
 package com.damon.vu.ppmtool.web;
 
 import com.damon.vu.ppmtool.domain.Project;
+import com.damon.vu.ppmtool.services.MapValidationErrorService;
 import com.damon.vu.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,22 +17,24 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/api/project")
 public class ProjectController {
+
+    @Autowired
     private ProjectService projectService;
 
-    // injecting the service using constructor
     @Autowired
-    public ProjectController(ProjectService theProjectService) {
-        this.projectService = theProjectService;
-    }
+    private MapValidationErrorService mapValidationErrorService;
+
+    public ProjectController() {}
 
     @PostMapping("")
     public ResponseEntity<?>createNewProject(@Valid @RequestBody Project theProject, BindingResult result) {
-        if (result.hasErrors()) {
-            return new ResponseEntity<String>("Invalid Project object", HttpStatus.BAD_REQUEST);
-        }
+
+        ResponseEntity<?> errorMap = mapValidationErrorService.mapError(result);
+        if (errorMap != null) return errorMap;
+
+        // otherwise
         // save the project to database
         Project p = projectService.saveOrUpdateProject(theProject);
-
         // returning the project as well as the status of the request
         return new ResponseEntity<Project>(p, HttpStatus.CREATED);
     }
